29e00d8202731b9c4cefb7672854ed561fc51689
bjayepps <epps@saucelabs.com>
2018-04-26 07:45:25 -0500

    test
    test

--- src/test/java/com/yourcompany/Tests/TestBase.java
+++ src/test/java/com/yourcompany/Tests/TestBase.java
@@ -135,7 +135,7 @@ public class TestBase implements SauceOnDemandSessionIdProvider {
         browsers.add(new String[]{"OS X 10.11", "latest", "safari", null, null});      
 
         
-        // extendedDebugging: True
+        // extendedDebugging: True;
         return browsers;
     }
 //test  
